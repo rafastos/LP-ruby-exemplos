5620920624e2e2dda7473dc0f11d89825ab44a47
Rafael S. Lima <rafaelkra2@gmail.com>
2024-10-20 10:35:05 -0300

    constants comentada

--- snake_game/constants.rb
+++ snake_game/constants.rb
@@ -1,9 +1,13 @@
-# Configurações do jogo
+# Configurações globais do jogo
+# Tamanho de cada célula da grade (em pixels)
 CELL_SIZE = 30
+# Número de células em cada direção (largura e altura)
 CELL_COUNT = 25
+# Margem ao redor do campo de jogo
 OFFSET = 75
+# Tamanho total da grade do jogo
 GRID_SIZE = CELL_SIZE * CELL_COUNT
 
-# Cores
-GREEN = '#ADC460'.freeze
-DARK_GREEN = '#2B3318'.freeze
+# Cores utilizadas no jogo
+GREEN = '#ADC460'      # Verde claro para o fundo
+DARK_GREEN = '#2B3318' # Verde escuro para a cobra e bordas
